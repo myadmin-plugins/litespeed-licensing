0a13ff392c46eaa9d76276c3dc060d3aa0d1bf90
Joe Huss <detain@interserver.net>
2017-06-07 07:41:11 -0400

    updates to settings

--- src/Plugin.php
+++ src/Plugin.php
@@ -77,6 +77,6 @@ class Plugin {
 		$settings = $event->getSubject();
 		$settings->add_text_setting('licenses', 'apisettings', 'litespeed_username', 'Litespeed Username:', 'Litespeed Username', $settings->get_setting('LITESPEED_USERNAME'));
 		$settings->add_text_setting('licenses', 'apisettings', 'litespeed_password', 'Litespeed Password:', 'Litespeed Password', $settings->get_setting('LITESPEED_PASSWORD'));
-		$settings->add_dropdown_setting('stock', 'outofstock_licenses_litespeed', 'Out Of Stock LiteSpeed Licenses', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LICENSES_LITESPEED'), array('0', '1'), array('No', 'Yes', ));
+		$settings->add_dropdown_setting('licenses', 'stock', 'outofstock_licenses_litespeed', 'Out Of Stock LiteSpeed Licenses', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LICENSES_LITESPEED'), array('0', '1'), array('No', 'Yes', ));
 	}
 }
