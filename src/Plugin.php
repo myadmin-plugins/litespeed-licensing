a5ccbf74de124988fc316bbd9d9fb3d52730250a
Joe Huss <detain@interserver.net>
2017-06-24 08:49:35 -0400

    refactoring module var

--- src/Plugin.php
+++ src/Plugin.php
@@ -19,11 +19,11 @@ class Plugin {
 
 	public static function getHooks() {
 		return [
-			'licenses.settings' => [__CLASS__, 'getSettings'],
-			'licenses.activate' => [__CLASS__, 'getActivate'],
-			'licenses.deactivate' => [__CLASS__, 'Deactivate'],
+			self::$module.'.settings' => [__CLASS__, 'getSettings'],
+			self::$module.'.activate' => [__CLASS__, 'getActivate'],
+			self::$module.'.deactivate' => [__CLASS__, 'Deactivate'],
 			'function.requirements' => [__CLASS__, 'getRequirements'],
-			'licenses.change_ip' => [__CLASS__, 'ChangeIp'],
+			self::$module.'.change_ip' => [__CLASS__, 'ChangeIp'],
 			'ui.menu' => [__CLASS__, 'getMenu'],
 		];
 	}
@@ -31,7 +31,7 @@ class Plugin {
 	public static function getActivate(GenericEvent $event) {
 		$license = $event->getSubject();
 		if ($event['category'] == SERVICE_TYPES_LITESPEED) {
-			myadmin_log('licenses', 'info', 'Litespeed Activation', __LINE__, __FILE__);
+			myadmin_log(self::$module, 'info', 'Litespeed Activation', __LINE__, __FILE__);
 			function_requirements('activate_litespeed');
 			$response = activate_litespeed($license->get_ip(), $event['field1'], $event['field2']);
 			if (isset($response['LiteSpeed_eService']['serial']))
@@ -43,7 +43,7 @@ class Plugin {
 	public static function Deactivate(GenericEvent $event) {
 		$license = $event->getSubject();
 		if ($event['category'] == SERVICE_TYPES_LITESPEED) {
-			myadmin_log('licenses', 'info', 'Litespeed Deactivation', __LINE__, __FILE__);
+			myadmin_log(self::$module, 'info', 'Litespeed Deactivation', __LINE__, __FILE__);
 			function_requirements('deactivate_litespeed');
 			deactivate_litespeed($license->get_ip());
 			$event->stopPropagation();
@@ -53,12 +53,12 @@ class Plugin {
 	public static function ChangeIp(GenericEvent $event) {
 		if ($event['category'] == SERVICE_TYPES_LITESPEED) {
 			$license = $event->getSubject();
-			$settings = get_module_settings('licenses');
+			$settings = get_module_settings(self::$module);
 			$litespeed = new \Litespeed(FANTASTICO_USERNAME, FANTASTICO_PASSWORD);
-			myadmin_log('licenses', 'info', "IP Change - (OLD:".$license->get_ip().") (NEW:{$event['newip']})", __LINE__, __FILE__);
+			myadmin_log(self::$module, 'info', "IP Change - (OLD:".$license->get_ip().") (NEW:{$event['newip']})", __LINE__, __FILE__);
 			$result = $litespeed->editIp($license->get_ip(), $event['newip']);
 			if (isset($result['faultcode'])) {
-				myadmin_log('licenses', 'error', 'Litespeed editIp('.$license->get_ip().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__);
+				myadmin_log(self::$module, 'error', 'Litespeed editIp('.$license->get_ip().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__);
 				$event['status'] = 'error';
 				$event['status_text'] = 'Error Code '.$result['faultcode'].': '.$result['fault'];
 			} else {
@@ -73,7 +73,7 @@ class Plugin {
 
 	public static function getMenu(GenericEvent $event) {
 		$menu = $event->getSubject();
-		$module = 'licenses';
+		$module = self::$module;
 		if ($GLOBALS['tf']->ima == 'admin') {
 			$menu->add_link($module.'api', 'choice=none.litespeed_list', 'whm/createacct.gif', 'List all Litespeed Licenses');
 		}
@@ -89,8 +89,8 @@ class Plugin {
 
 	public static function getSettings(GenericEvent $event) {
 		$settings = $event->getSubject();
-		$settings->add_text_setting('licenses', 'Litespeed', 'litespeed_username', 'Litespeed Username:', 'Litespeed Username', $settings->get_setting('LITESPEED_USERNAME'));
-		$settings->add_text_setting('licenses', 'Litespeed', 'litespeed_password', 'Litespeed Password:', 'Litespeed Password', $settings->get_setting('LITESPEED_PASSWORD'));
-		$settings->add_dropdown_setting('licenses', 'Litespeed', 'outofstock_licenses_litespeed', 'Out Of Stock LiteSpeed Licenses', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LICENSES_LITESPEED'), array('0', '1'), array('No', 'Yes', ));
+		$settings->add_text_setting(self::$module, 'Litespeed', 'litespeed_username', 'Litespeed Username:', 'Litespeed Username', $settings->get_setting('LITESPEED_USERNAME'));
+		$settings->add_text_setting(self::$module, 'Litespeed', 'litespeed_password', 'Litespeed Password:', 'Litespeed Password', $settings->get_setting('LITESPEED_PASSWORD'));
+		$settings->add_dropdown_setting(self::$module, 'Litespeed', 'outofstock_licenses_litespeed', 'Out Of Stock LiteSpeed Licenses', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LICENSES_LITESPEED'), array('0', '1'), array('No', 'Yes', ));
 	}
 }
