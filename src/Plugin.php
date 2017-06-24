efe789c8ad5ca2c79c5357bccd8bc8c3e7616a63
Joe Huss <detain@interserver.net>
2017-06-24 06:19:04 -0400

    menu camelCase refactoring

--- src/Plugin.php
+++ src/Plugin.php
@@ -24,7 +24,7 @@ class Plugin {
 			'licenses.deactivate' => [__CLASS__, 'Deactivate'],
 			'function.requirements' => [__CLASS__, 'getRequirements'],
 			'licenses.change_ip' => [__CLASS__, 'ChangeIp'],
-			'ui.menu' => [__CLASS__, 'Menu'],
+			'ui.menu' => [__CLASS__, 'getMenu'],
 		];
 	}
 
@@ -72,7 +72,7 @@ class Plugin {
 		}
 	}
 
-	public static function Menu(GenericEvent $event) {
+	public static function getMenu(GenericEvent $event) {
 		// will be executed when the licenses.settings event is dispatched
 		$menu = $event->getSubject();
 		$module = 'licenses';
