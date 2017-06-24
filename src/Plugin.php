1ad9a34a30c3cd9fe04a7f54f9145027c296b9ca
Joe Huss <detain@interserver.net>
2017-06-24 16:24:15 -0400

    lots of fixs and updates .. putting things in their proper place and adding some of the activation thooks and such

--- src/Plugin.php
+++ src/Plugin.php
@@ -40,7 +40,7 @@ class Plugin {
 		}
 	}
 
-	public static function getgetDeactivate(GenericEvent $event) {
+	public static function getDeactivate(GenericEvent $event) {
 		$license = $event->getSubject();
 		if ($event['category'] == SERVICE_TYPES_LITESPEED) {
 			myadmin_log(self::$module, 'info', 'Litespeed Deactivation', __LINE__, __FILE__);
