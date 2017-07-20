3a7334aaa8112c49562d9090c17714f1c19603ef
Joe Huss <detain@interserver.net>
2017-07-20 01:30:28 -0400

    changing to single quotes

--- bin/activate_litespeed.php
+++ bin/activate_litespeed.php
@@ -5,7 +5,7 @@ $GLOBALS['tf']->session->create(160308, 'services');
 $GLOBALS['tf']->session->verify();
 
 $response = activate_litespeed('1.2.3.4', 'LSWS', 1);
-echo "Response: ";
+echo 'Response: ';
 var_export($response);
 echo "\n";
 //deactivate_cpanel('66.45.228.100');
