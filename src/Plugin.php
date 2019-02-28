e608f2c36f180084414c8abf640316b689100df7
Joe Huss <detain@interserver.net>
2019-02-28 11:09:06 -0500

    updates aincorperating the module parameter into the myadmin_log calls

--- src/Plugin.php
+++ src/Plugin.php
@@ -91,7 +91,7 @@ class Plugin
 			$result = activate_litespeed($event['newip'], $event['field1'], $event['field2']);
 			//$result = $litespeed->editIp($serviceClass->getIp(), $event['newip']);
 			if (isset($result['faultcode'])) {
-				myadmin_log(self::$module, 'error', 'LiteSpeed editIp('.$serviceClass->getIp().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__);
+				myadmin_log(self::$module, 'error', 'LiteSpeed editIp('.$serviceClass->getIp().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__, self::$module);
 				$event['status'] = 'error';
 				$event['status_text'] = 'Error Code '.$result['faultcode'].': '.$result['fault'];
 			} else {
