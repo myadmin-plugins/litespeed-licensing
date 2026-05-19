8698aa04467dbbfa9c76b545ebde27a31a194dee
Joe Huss <detain@interserver.net>
2026-05-19 12:44:48 -0400

    updates to cli scripts fixing ima sesison handling

--- bin/activate_litespeed.php
+++ bin/activate_litespeed.php
@@ -1,13 +1,13 @@
 <?php
 
 require_once __DIR__.'/../../../../include/functions.inc.php';
-\MyAdmin\App::session()->create(160308, 'services');
-\MyAdmin\App::session()->verify();
+\MyAdmin\App::session()->sessionid = substr(basename($_SERVER['argv'][0], '.php'), 0, 32);
+\MyAdmin\App::session()->account_id = 160308;
+\MyAdmin\App::session()->appnocache('ima', 'services');
+\MyAdmin\App::tf()->ima = 'services';
 
 $response = activate_litespeed('1.2.3.4', 'LSWS', 1);
 echo 'Response: ';
 var_export($response);
 echo "\n";
 //deactivate_cpanel('66.45.228.100');
-
-\MyAdmin\App::session()->destroy();
